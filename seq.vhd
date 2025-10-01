b430e94734f710ed6419b24b1ba23d2d822055d9
fetohiaras <theo.farias@gmail.com>
2025-10-01 16:00:35 +0200

    corrected state machine to comply with correct fsm structure

--- seq.vhd
+++ seq.vhd
@@ -2,181 +2,209 @@ library ieee;
 use ieee.std_logic_1164.all;
 
 entity seq is
-    port (
-        clk      : in  std_logic;
-        reset    : in  std_logic;
-        enread   : in  std_logic;
-        enwrite  : in  std_logic;
-        req      : in  std_logic;
-        ack      : out std_logic;
-        rw_n     : out std_logic;
-        oe       : out std_logic;
-        incwrite : out std_logic;
-        incread  : out std_logic;
-        hl       : out std_logic;
-        selread  : out std_logic;
-        cs_n     : out std_logic
-    );
+  port (
+    clk      : in  std_logic;
+    reset    : in  std_logic;
+    enread   : in  std_logic;
+    enwrite  : in  std_logic;
+    req      : in  std_logic;
+
+    ack      : out std_logic;
+    rw_n     : out std_logic;
+    oe       : out std_logic;
+    incwrite : out std_logic;
+    incread  : out std_logic;
+    hl       : out std_logic;
+    selread  : out std_logic;
+    cs_n     : out std_logic
+  );
 end seq;
 
+-- moore state machine
 architecture archi_moore of seq is
-    type state_type is (repos, ecrire, lect1, lect2, attente);
-    signal current_state, next_state : state_type := repos;
+  type state_type is (repos, ecrire, lect1, lect2, attente);
+  signal current_state, next_state : state_type := repos;
 begin
-    process(clk)
-    begin
-        if rising_edge(clk) then
-            if reset = '1' then
-                current_state <= repos;
-            else
-                current_state <= next_state;
-            end if;
-        end if;
-    end process;
+  -- state register (sync)
+  process(clk)
+  begin
+    if rising_edge(clk) then
+      if reset = '1' then
+        current_state <= repos;
+      else
+        current_state <= next_state;
+      end if;
+    end if;
+  end process;
 
-    process(current_state, enread, enwrite, req, reset)
-    begin
-        if reset = '1' then
-            next_state <= repos;
+  -- next state decoder
+  process(current_state, enread, enwrite, req)
+  begin
+    next_state <= current_state;  -- default
+    case current_state is
+      when repos =>
+        if enread = '1' then
+          next_state <= lect1;
+        elsif (enread = '0' and req = '0' and enwrite = '1') then
+          next_state <= ecrire;
         else
-            case current_state is
-                when repos =>
-                    if enread = '1' then
-                        next_state <= lect1;
-                    elsif enwrite = '1' and req = '1' then
-                        next_state <= ecrire;
-                    else
-                        next_state <= repos;
-                    end if;
-                when lect1 =>
-                    next_state <= lect2;
-                when lect2 =>
-                    if req = '1' then
-                        next_state <= attente;
-                    else
-                        next_state <= repos;
-                    end if;
-                when ecrire =>
-                    next_state <= repos;
-                when attente =>
-                    if enwrite = '1' then
-                        next_state <= ecrire;
-                    else
-                        next_state <= repos;
-                    end if;
-            end case;
+          next_state <= repos;
         end if;
-    end process;
 
-    process(current_state)
-    begin
-        case current_state is
-            when repos =>
-                ack <= '1';
-                rw_n <= '1';
-                oe <= '0';
-                incwrite <= '0';
-                incread <= '0';
-                hl <= '0';
-                selread <= '0';
-                cs_n <= '1';
-            when ecrire =>
-                ack <= '1';
-                rw_n <= '0';
-                oe <= '0';
-                incwrite <= '1';
-                incread <= '0';
-                hl <= '0';
-                selread <= '0';
-                cs_n <= '0';
-            when lect1 =>
-                ack <= '0';
-                rw_n <= '1';
-                oe <= '1';
-                incwrite <= '0';
-                incread <= '1';
-                hl <= '1';
-                selread <= '1';
-                cs_n <= '0';
-            when lect2 =>
-                ack <= '0';
-                rw_n <= '1';
-                oe <= '1';
-                incwrite <= '0';
-                incread <= '0';
-                hl <= '0';
-                selread <= '1';
-                cs_n <= '0';
-            when attente =>
-                ack <= '0';
-                rw_n <= '1';
-                oe <= '0';
-                incwrite <= '0';
-                incread <= '0';
-                hl <= '0';
-                selread <= '1';
-                cs_n <= '1';
-        end case;
-    end process;
+      when ecrire =>
+        next_state <= attente;  
+
+      when attente =>
+        if enread = '1' then
+          next_state <= lect2;
+        elsif (enread = '0' and req = '1') then
+          next_state <= repos;
+        else  -- enread=0 and req=0
+          next_state <= attente;
+        end if;
+
+      when lect1 =>
+        next_state <= repos;    
+
+      when lect2 =>
+        next_state <= attente;  
+    end case;
+  end process;
+
+  -- per state outputs
+  process(current_state)
+  begin
+    -- defaults
+    ack      <= '0';
+    rw_n     <= '1';
+    oe       <= '0';
+    incwrite <= '0';
+    incread  <= '0';
+    hl       <= '0';
+    selread  <= '0';
+    cs_n     <= '1';
+
+    case current_state is
+      when repos =>
+        ack      <= '1';
+        selread  <= '0';
+        cs_n     <= '1';
+
+      when ecrire =>
+        rw_n     <= '0';
+        incwrite <= '1';
+        cs_n     <= '0';
+
+      when lect1 =>
+        ack      <= '1';
+        oe       <= '1';
+        incread  <= '1';
+        hl       <= '1';
+        selread  <= '1';
+        cs_n     <= '0';
+
+      when lect2 =>
+        oe       <= '1';
+        incread  <= '1';  
+        hl       <= '1';  
+        selread  <= '1';
+        cs_n     <= '0';
+
+      when attente =>
+        selread  <= '1';
+        cs_n     <= '1';
+    end case;
+  end process;
 
 end archi_moore;
 
+--mealy fsm
 architecture archi_mealy of seq is
-    type state_type is (repos, ecrire, lect1, lect2, attente);
-    signal current_state, next_state : state_type := repos;
+  type state_type is (repos, ecrire, lect1, lect2, attente);
+  signal current_state, next_state : state_type := repos;
 begin
-    process(clk)
-    begin
-        if rising_edge(clk) then
-            if reset = '1' then
-                current_state <= repos;
-            else
-                current_state <= next_state;
-            end if;
-        end if;
-    end process;
+  -- state register
+  process(clk)
+  begin
+    if rising_edge(clk) then
+      if reset = '1' then
+        current_state <= repos;
+      else
+        current_state <= next_state;
+      end if;
+    end if;
+  end process;
 
-    process(current_state, enread, enwrite, req, reset)
-    begin
-        if reset = '1' then
-            next_state <= repos;
+  -- next state decoder
+  process(current_state, enread, enwrite, req)
+  begin
+    next_state <= current_state;
+    case current_state is
+      when repos =>
+        if enread = '1' then
+          next_state <= lect1;
+        elsif (enread = '0' and req = '0' and enwrite = '1') then
+          next_state <= ecrire;
         else
-            case current_state is
-                when repos =>
-                    if enread = '1' then
-                        next_state <= lect1;
-                    elsif enwrite = '1' and req = '1' then
-                        next_state <= ecrire;
-                    else
-                        next_state <= repos;
-                    end if;
-                when lect1 =>
-                    next_state <= lect2;
-                when lect2 =>
-                    if req = '1' then
-                        next_state <= attente;
-                    else
-                        next_state <= repos;
-                    end if;
-                when ecrire =>
-                    next_state <= repos;
-                when attente =>
-                    if enwrite = '1' then
-                        next_state <= ecrire;
-                    else
-                        next_state <= repos;
-                    end if;
-            end case;
+          next_state <= repos;
         end if;
-    end process;
 
-    ack <= '1' when ((current_state = repos or current_state = ecrire) and enwrite = '1' and not enread = '1') else '0';
-    rw_n <= '1' when (current_state = lect1 or current_state = lect2 or current_state = attente) else '0';
-    oe <= '1' when ((current_state = lect1 or current_state = lect2) and enread = '1') else '0';
-    incwrite <= '1' when (current_state = ecrire and enwrite = '1') else '0';
-    incread <= '1' when (current_state = lect1 and enread = '1') else '0';
-    hl <= '1' when ((current_state = lect1 or current_state = lect2) and enread = '1') else '0';
-    selread <= '1' when (current_state = lect1 or current_state = lect2 or current_state = attente) else '0';
-    cs_n <= '0' when (current_state = ecrire or current_state = lect1 or current_state = lect2) else '1';
+      when ecrire =>
+        next_state <= attente;
+
+      when attente =>
+        if enread = '1' then
+          next_state <= lect2;
+        elsif (enread = '0' and req = '1') then
+          next_state <= repos;
+        else
+          next_state <= attente;
+        end if;
+
+      when lect1 =>
+        next_state <= repos;
+
+      when lect2 =>
+        next_state <= attente;
+    end case;
+  end process;
+
+  -- outputs per state
+  process(current_state, enread, enwrite, req)
+  begin
+    -- defaults
+    ack      <= '0';
+    rw_n     <= '1';
+    oe       <= '0';
+    incwrite <= '0';
+    incread  <= '0';
+    hl       <= '0';
+    selread  <= '0';
+    cs_n     <= '1';
+
+    case current_state is
+      when repos =>
+        if not (enread = '0' and req = '0' and enwrite = '1') then
+          ack <= '1';
+        end if;
+        selread <= '0';
+        cs_n    <= '1';
+
+      when ecrire =>
+        rw_n     <= '0'; incwrite <= '1'; cs_n <= '0';
+
+      when lect1 =>
+        ack      <= '1';
+        oe       <= '1'; incread <= '1'; hl <= '1';
+        selread  <= '1'; cs_n <= '0';
+
+      when lect2 =>
+        oe       <= '1'; incread <= '1'; hl <= '1';
+        selread  <= '1'; cs_n <= '0';
+
+      when attente =>
+        selread  <= '1'; cs_n <= '1';
+    end case;
+  end process;
 
 end archi_mealy;
--- tb_seq.vhd
+++ tb_seq.vhd
@@ -1,135 +1,202 @@
 library ieee;
 use ieee.std_logic_1164.all;
-use std.textio.all;
 
 entity tb_seq is
 end tb_seq;
 
 architecture archi_tb_seq of tb_seq is
-    component seq
-        port (
-            clk      : in  std_logic;
-            reset    : in  std_logic;
-            enread   : in  std_logic;
-            enwrite  : in  std_logic;
-            req      : in  std_logic;
-            ack      : out std_logic;
-            rw_n     : out std_logic;
-            oe       : out std_logic;
-            incwrite : out std_logic;
-            incread  : out std_logic;
-            hl       : out std_logic;
-            selread  : out std_logic;
-            cs_n     : out std_logic
-        );
-    end component;
+  component seq
+    port (
+      clk      : in  std_logic;
+      reset    : in  std_logic;
+      enread   : in  std_logic;
+      enwrite  : in  std_logic;
+      req      : in  std_logic;
 
-    constant CLK_PERIOD : time := 100 ns;  -- 10 MHz
+      ack      : out std_logic;
+      rw_n     : out std_logic;
+      oe       : out std_logic;
+      incwrite : out std_logic;
+      incread  : out std_logic;
+      hl       : out std_logic;
+      selread  : out std_logic;
+      cs_n     : out std_logic
+    );
+  end component;
 
-    signal clk_tb      : std_logic := '0';
-    signal reset_tb    : std_logic := '1';
-    signal enread_tb   : std_logic := '0';
-    signal enwrite_tb  : std_logic := '1';  -- Default high, as per GENHL (high most time)
-    signal req_tb      : std_logic := '0';
-    signal ack_tb      : std_logic;
-    signal rw_n_tb     : std_logic;
-    signal oe_tb       : std_logic;
-    signal incwrite_tb : std_logic;
-    signal incread_tb  : std_logic;
-    signal hl_tb       : std_logic;
-    signal selread_tb  : std_logic;
-    signal cs_n_tb     : std_logic;
+  constant CLK_PERIOD : time := 100 ns;
+
+  signal clk_tb      : std_logic := '0';
+  signal reset_tb    : std_logic := '1';
+  signal enread_tb   : std_logic := '0';
+  signal enwrite_tb  : std_logic := '0';
+  signal req_tb      : std_logic := '1';  -- default: no immediate write request
+
+  signal ack_tb      : std_logic;
+  signal rw_n_tb     : std_logic;
+  signal oe_tb       : std_logic;
+  signal incwrite_tb : std_logic;
+  signal incread_tb  : std_logic;
+  signal hl_tb       : std_logic;
+  signal selread_tb  : std_logic;
+  signal cs_n_tb     : std_logic;
+
+  signal done        : std_logic := '0';  -- stop the clock for a clean finish
 
 begin
-    -- Instantiate DUT (will use configuration for archi_moore or archi_mealy)
-    dut: seq
-        port map (
-            clk      => clk_tb,
-            reset    => reset_tb,
-            enread   => enread_tb,
-            enwrite  => enwrite_tb,
-            req      => req_tb,
-            ack      => ack_tb,
-            rw_n     => rw_n_tb,
-            oe       => oe_tb,
-            incwrite => incwrite_tb,
-            incread  => incread_tb,
-            hl       => hl_tb,
-            selread  => selread_tb,
-            cs_n     => cs_n_tb
-        );
+  -- select fsm architecture here
+  dut: entity work.seq(archi_moore)
+    port map (
+      clk      => clk_tb,
+      reset    => reset_tb,
+      enread   => enread_tb,
+      enwrite  => enwrite_tb,
+      req      => req_tb,
+      ack      => ack_tb,
+      rw_n     => rw_n_tb,
+      oe       => oe_tb,
+      incwrite => incwrite_tb,
+      incread  => incread_tb,
+      hl       => hl_tb,
+      selread  => selread_tb,
+      cs_n     => cs_n_tb
+    );
 
-    -- Clock generator
-    clk_process: process
+  -- controlled clock (stops with done=1)
+  clk_proc : process
+  begin
+    while done = '0' loop
+      clk_tb <= '0'; wait for CLK_PERIOD/2;
+      clk_tb <= '1'; wait for CLK_PERIOD/2;
+    end loop;
+    wait;
+  end process;
+
+  -- Stimuli and checks
+  stimulus : process
+    -- helper procedures: wait for N rising edges
+    procedure wait_cycles(n : natural) is
     begin
-        clk_tb <= '0';
-        wait for CLK_PERIOD / 2;
-        clk_tb <= '1';
-        wait for CLK_PERIOD / 2;
-    end process;
+      for i in 1 to n loop
+        wait until rising_edge(clk_tb);
+      end loop;
+    end procedure;
 
-    -- Stimulus process
-    stimulus: process
+    -- helper: drive enread='1' for 1 clock cycle
+    procedure pulse_enread_1clk is
     begin
-        -- Initial reset (stay in repos)
-        reset_tb <= '1';
-        wait for 5 * CLK_PERIOD;
-        reset_tb <= '0';
-        wait for CLK_PERIOD;
-        -- Check repos: ack=1, others=0 (approx)
-        assert ack_tb = '1' report "Ack not 1 in repos" severity error;
-        assert incwrite_tb = '0' report "Incwrite not 0 in repos" severity error;
-        assert incread_tb = '0' report "Incread not 0 in repos" severity error;
-        assert oe_tb = '0' report "OE not 0 in repos" severity error;
-        wait for CLK_PERIOD;
+      enread_tb <= '1';
+      wait until rising_edge(clk_tb);
+      enread_tb <= '0';
+    end procedure;
 
-        -- Test read priority: enread=1, ignore req (set req=1 too)
-        req_tb <= '1';
-        enread_tb <= '1';
-        enwrite_tb <= '0';  -- During read slot
-        wait for CLK_PERIOD;  -- Go to lect1
-        assert ack_tb = '0' report "Ack not delayed in lect1" severity error;
-        assert rw_n_tb = '1' report "RW_n not 1 in lect1" severity error;
-        assert oe_tb = '1' report "OE not 1 in lect1" severity error;
-        assert incread_tb = '1' report "Incread not 1 in lect1" severity error;
-        assert hl_tb = '1' report "HL not 1 in lect1" severity error;
-        assert selread_tb = '1' report "Selread not 1 in lect1" severity error;
-        assert cs_n_tb = '0' report "CS_n not 0 in lect1" severity error;
-        wait for CLK_PERIOD;  -- Go to lect2
-        assert ack_tb = '0' report "Ack not 0 in lect2" severity error;
-        assert oe_tb = '1' report "OE not 1 in lect2 (Mealy may differ)" severity note;
-        assert selread_tb = '1' report "Selread not 1 in lect2" severity error;
-        enread_tb <= '0';
-        enwrite_tb <= '1';
-        wait for CLK_PERIOD;  -- Go to attente (since req=1)
-        assert ack_tb = '0' report "Ack not 0 in attente" severity error;
-        assert selread_tb = '1' report "Selread not 1 in attente" severity error;
-        wait for CLK_PERIOD;  -- Go to ecrire (enwrite=1)
-        assert ack_tb = '1' report "Ack not 1 in ecrire" severity error;
-        assert rw_n_tb = '0' report "RW_n not 0 in ecrire" severity error;
-        assert incwrite_tb = '1' report "Incwrite not 1 in ecrire" severity error;
-        assert cs_n_tb = '0' report "CS_n not 0 in ecrire" severity error;
-        req_tb <= '0';  -- End req
-        wait for CLK_PERIOD;  -- Back to repos
-        assert ack_tb = '1' report "Ack not 1 back to repos" severity error;
-        wait for 2 * CLK_PERIOD;
+  begin
+    -- set to repos
+    enread_tb  <= '0';
+    enwrite_tb <= '0';
+    req_tb     <= '1';         -- no immediate write
+    reset_tb   <= '1';
+    wait_cycles(2);
+    reset_tb   <= '0';
+    wait until rising_edge(clk_tb);  -- now in repos
 
-        -- Test write from repos (no read pending)
-        req_tb <= '1';
-        wait for CLK_PERIOD;  -- Stay repos (enread=0, but enwrite=1, req=1 -> go to ecrire next)
-        wait for CLK_PERIOD;  -- In ecrire
-        assert incwrite_tb = '1' report "Incwrite not 1 in ecrire" severity error;
-        assert ack_tb = '1' report "Ack not 1 in ecrire" severity error;
-        req_tb <= '0';
-        wait for CLK_PERIOD;  -- Back to repos
-        wait for 2 * CLK_PERIOD;
+    -- repos checks (Moore)
+    assert ack_tb      = '1' report "Repos: ack/=1" severity error;
+    assert rw_n_tb     = '1' report "Repos: rw_n/=1" severity error;
+    assert oe_tb       = '0' report "Repos: oe/=0" severity error;
+    assert incwrite_tb = '0' report "Repos: incwrite/=0" severity error;
+    assert incread_tb  = '0' report "Repos: incread/=0" severity error;
+    assert hl_tb       = '0' report "Repos: hl/=0" severity error;
+    assert selread_tb  = '0' report "Repos: selread/=0" severity error;
+    assert cs_n_tb     = '1' report "Repos: cs_n/=1" severity error;
 
-        -- Test idle in repos (no enread, no req)
-        assert ack_tb = '1' report "Ack not 1 in idle repos" severity error;
-        wait for 5 * CLK_PERIOD;
+    -- scenario 1: test enread waiting 200 cycles (as expected from GENHL)
+    wait_cycles(200);
 
-        report "Testbench completed - verify waveform for states/transitions" severity note;
-        wait;
-    end process;
+    pulse_enread_1clk;               -- enter lect1 on this cycle
+
+    -- check lect1 during the pulse
+    assert ack_tb      = '1' report "Lect1: ack/=1" severity error;
+    assert rw_n_tb     = '1' report "Lect1: rw_n/=1" severity error;
+    assert oe_tb       = '1' report "Lect1: oe/=1" severity error;
+    assert incread_tb  = '1' report "Lect1: incread/=1" severity error;
+    assert hl_tb       = '1' report "Lect1: hl/=1" severity error;
+    assert selread_tb  = '1' report "Lect1: selread/=1" severity error;
+    assert cs_n_tb     = '0' report "Lect1: cs_n/=0" severity error;
+
+    wait until rising_edge(clk_tb);  -- back to repos
+    assert ack_tb      = '1' report "Back to Repos after Lect1: ack/=1" severity error;
+    assert selread_tb  = '0' report "Back to Repos after Lect1: selread/=0" severity error;
+
+    --scenario 2: write, wait x cycles (mimic genhl) and generate read pulse 
+    --expected: repos->ecrire->attente->lect2->attente->repos
+    -- trigger writing (condition: enread=0, enwrite=1, req=0)
+    enread_tb  <= '0';
+    enwrite_tb <= '1';
+    req_tb     <= '0';
+    wait until rising_edge(clk_tb);  -- Ecrire
+
+    -- ecrire checks
+    assert rw_n_tb     = '0' report "Ecrire: rw_n/=0" severity error;
+    assert incwrite_tb = '1' report "Ecrire: incwrite/=1" severity error;
+    assert cs_n_tb     = '0' report "Ecrire: cs_n/=0" severity error;
+    assert ack_tb      = '0' report "Ecrire: ack/=0 (Moore)" severity error;
+
+    wait until rising_edge(clk_tb);  -- attente
+
+    -- attente checks
+    assert selread_tb  = '1' report "Attente: selread/=1" severity error;
+    assert cs_n_tb     = '1' report "Attente: cs_n/=1" severity error;
+    assert ack_tb      = '0' report "Attente: ack/=0 (Moore)" severity error;
+
+    -- wait X cycles (< 200 here)
+    wait_cycles(50);
+
+    -- one read pulse -> lect2
+    pulse_enread_1clk;
+
+    -- lect2 checks
+    assert oe_tb       = '1' report "Lect2: oe/=1" severity error;
+    assert incread_tb  = '1' report "Lect2: incread/=1" severity error;
+    assert hl_tb       = '1' report "Lect2: hl/=1 (errata)" severity error;
+    assert selread_tb  = '1' report "Lect2: selread/=1" severity error;
+    assert cs_n_tb     = '0' report "Lect2: cs_n/=0" severity error;
+    assert ack_tb      = '0' report "Lect2: ack/=0 (Moore)" severity error;
+
+    -- end of pulse → back to attente for 1 cycle
+    wait until rising_edge(clk_tb);
+    assert selread_tb  = '1' report "Back to Attente: selread/=1" severity error;
+    assert cs_n_tb     = '1' report "Back to Attente: cs_n/=1" severity error;
+
+    -- exit attente: enread=0 and req=1 -> repos
+    req_tb <= '1';
+    wait until rising_edge(clk_tb);  -- repos
+    assert ack_tb     = '1' report "Back to Repos after Attente: ack/=1" severity error;
+    assert selread_tb = '0' report "Back to Repos after Attente: selread/=0" severity error;
+
+    --scenario 3: test reset interrupting write cycle in attente
+    --repos->ecrire->attente->reset->repos with enread still at 1
+    -- prepare another write
+    enread_tb  <= '0';
+    enwrite_tb <= '1';
+    req_tb     <= '0';
+    wait until rising_edge(clk_tb);  -- ecrire
+    wait until rising_edge(clk_tb);  -- attente
+
+    -- synchronous reset while in attente
+    reset_tb <= '1';
+    wait until rising_edge(clk_tb);  -- on this edge fsm must go to repos
+    reset_tb <= '0';
+
+    -- confirm repos right after reset (no enread needed)
+    assert ack_tb     = '1' report "Reset from Attente: not Repos (ack/=1)" severity error;
+    assert selread_tb = '0' report "Reset from Attente: selread/=0" severity error;
+    assert cs_n_tb    = '1' report "Reset from Attente: cs_n/=1" severity error;
+
+    ------------------------------------------------------------------
+    report "TB seq: 3 scenarios completed without errors (Moore FSM). For Mealy, 'ack' in Repos state may differ." severity note;
+    done <= '1';
+    wait;
+  end process;
 
 end archi_tb_seq;
